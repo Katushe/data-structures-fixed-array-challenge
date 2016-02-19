37a048451f9778f9cd33e8141608a09758dcc091
Katushe <kgarbacz11@gmail.com>
2016-02-18 22:31:22 -0600

    Create FixedArray class + tests

--- source/fixed_array.rb
+++ source/fixed_array.rb
@@ -1,3 +1,21 @@
 class FixedArray
+  attr_reader :array
 
+  class OutOfBoundsException < StandardError
+  end
+
+  def initialize(size)
+    @array = Array.new(size)
+  end
+
+  def get(index)
+    raise OutOfBoundsException, "That index is out of the bounds of this array" if index >= @array.length
+    return @array[index]
+  end
+
+  def set(index, element)
+    raise OutOfBoundsException, "That index is out of the bounds of this array" if index >= @array.length
+    @array[index] = element
+  end
 end
+
--- source/fixed_array_spec.rb
+++ source/fixed_array_spec.rb
@@ -1,5 +1,31 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
-  
+ let(:fixed_array) {FixedArray.new(5)}
+
+  it "creates an array of the provided size" do
+    expect(fixed_array.array.length).to eq(5)
+  end
+
+  describe "#gets" do
+    it "gets the correct value from an index" do
+      fixed_array.array[2] = 4
+      expect(fixed_array.get(2)).to eq(4)
+    end
+
+    it "raises an error if you try to access an index past the size of the array" do
+      expect{fixed_array.get(6)}.to raise_error(FixedArray::OutOfBoundsException)
+    end
+  end
+
+
+  describe "#set" do
+    it "sets a value to the correct index" do
+      expect{fixed_array.set(3, 6)}.to change{fixed_array.array[3]}.to(6)
+    end
+
+    it "raises an error if you try to assign a value to an index past the size of the array" do
+      expect{fixed_array.set(7, 6)}.to raise_error(FixedArray::OutOfBoundsException)
+    end
+  end
 end
